5ad9367683e8c93deb5a5952d5751c5235dc1577
pa.sheludyakov <pasha.technician@bk.ru>
2026-04-01 23:05:20 +0300

    chore: remove unused env variables from .env

--- authMicro/db/migrations/000002_pg_cron_cleanup.down.sql
+++ authMicro/db/migrations/000002_pg_cron_cleanup.down.sql
@@ -1,4 +1,5 @@
 SELECT cron.unschedule('cleanup_registration_sessions');
 SELECT cron.unschedule('cleanup_login_session');
 SELECT cron.unschedule('cleanup_token_session');
-DROP EXTENSION IF EXISTS pg_cron;
+
+SELECT * FROM cron.job;
--- authMicro/db/migrations/000002_pg_cron_cleanup.up.sql
+++ authMicro/db/migrations/000002_pg_cron_cleanup.up.sql
@@ -1,7 +1,8 @@
-CREATE EXTENSION IF NOT EXISTS pg_cron;
-SELECT cron.schedule('cleanup_registration_sessions', '*/1 * * * *',
+SELECT cron.schedule('cleanup_registration_sessions', '* */3 * * *',
                      $$ DELETE FROM registration_session WHERE code_expires < NOW() $$);
-SELECT cron.schedule('cleanup_login_session', '*/1 * * * *',
+SELECT cron.schedule('cleanup_login_session', '* */5 * * *',
                      $$ DELETE FROM login_session WHERE code_expires < NOW() $$);
-SELECT cron.schedule('cleanup_token_session', '*/1 * * * *',
+SELECT cron.schedule('cleanup_token_session', '30 */2 * * *',
                      $$ DELETE FROM refresh_token_session WHERE expires_at < NOW() $$);
+
+SELECT * FROM cron.job;
